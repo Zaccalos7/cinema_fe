ed2eb23b98243bf52bafb93b00e4192b17922433
Marco Guarino <guarin719@outlook.it>
2025-12-03 18:02:22 +0100

    dummy change

--- server.mjs
+++ server.mjs
@@ -3,6 +3,9 @@ import express from 'express'
 const app = express()
 
 const port = process.env.PORT || 3000
+
+console.log(port)
+
 app.listen(port, () => {
     console.log(`🚀 Running NON-SECURE server (${port} - ${process.env.NODE_ENV})`)
   })
