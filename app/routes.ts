7015fe57f8454ff65a10f25a9ff0d83eaa7c63fa
Marco Guarino <guarin719@outlook.it>
2025-12-03 14:28:32 +0100

    basic routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,32 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
-//TO_DO 3
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+import { type RouteConfig, index } from "@react-router/dev/routes"
+import {remixConfigRoutes} from '@react-router/remix-config-routes-adapter'
+import fs from 'fs'
+
+type YAMLProjectContent = {
+    active: boolean
+    locales: string[]
+}
+
+const getFilesFromPath = (path:string, extension: string) => {
+    const files = fs.readdirSync(path)
+    return files.filter(file => file.match(new RegExp(`.*.(${extension})`, 'ig')))
+}
+
+const getLists = async ()=>{
+    const PROJECT_LIST : {list: {[key: string]: YAMLProjectContent}} = {list : {}}
+    const projectFiles = [
+        ...getFilesFromPath('projects_config', "yaml")
+    ]
+    console.log({projectFiles})
+}
+
+const pippo = await getLists()
+
+const routes = remixConfigRoutes(defineRoutes => {
+  const mergedRoutes = {}
+
+ 
+  return mergedRoutes
+})
+
+export default routes satisfies RouteConfig
