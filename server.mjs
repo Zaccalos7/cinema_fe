bb8ac7b39c0f19312da480a12fdad84a1ab9c1c6
Marco Guarino <guarin719@outlook.it>
2025-12-03 17:52:49 +0100

    added server.mjs

---
+++ server.mjs
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`🚀 Running NON-SECURE server (${port} - ${process.env.NODE_ENV})`)
+  })
