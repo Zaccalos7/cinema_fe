4fafdbbae235c01e90e7c38ebb38b8f1ae1bca39
Marco Guarino <guarin719@outlook.it>
2025-12-04 09:56:17 +0100

    other config

---
+++ env.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+import '@remix-run/server-runtime'
+
+declare module '@remix-run/server-runtime' {
+  export interface AppLoadContext {
+    session: {
+      get: (key: string) => any
+      set: (key: string, value: any) => boolean
+      unset: (key: string) => boolean
+      has: (key: string) => boolean
+      flash: (key: string, value: any) => void
+    }
+  }
+}
