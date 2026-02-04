52971ad83c581223e8fc5349759d5f1ffc43bf8c
Marco Guarino <guarin719@outlook.it>
2026-02-04 16:41:46 +0100

    added style on NewDiv

--- app/components/NewDiv.tsx
+++ app/components/NewDiv.tsx
@@ -3,12 +3,15 @@ import {cn} from '~/libs/cn'
 type NewDivProps = {
   className?: string
   children?: React.ReactNode
+  style?: React.CSSProperties
 }
 
-const NewDiv = ({className = '', children}: NewDivProps) => {
+const NewDiv = ({className = '', children, style}: NewDivProps) => {
   return (
     <div className="contents">
-      <div className={cn(className)}>{children}</div>
+      <div className={cn(className)} style={{...style}}>
+        {children}
+      </div>
     </div>
   )
 }
