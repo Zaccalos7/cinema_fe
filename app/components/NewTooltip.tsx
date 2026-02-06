d48be6ae982a32209df9b4d3f6446761bbde607f
Marco Guarino <guarin719@outlook.it>
2026-02-06 19:27:38 +0100

    added newTooltip base

---
+++ app/components/NewTooltip.tsx
@@ -0,0 +1,10 @@
+interface NewTooltipProps {
+  children: React.ReactNode | string
+  tooltipContent: string | React.ReactNode | null
+  maxWidth?: string | number
+  asChild?: boolean
+}
+
+const NewTooltip = () => {}
+
+export default NewTooltip
