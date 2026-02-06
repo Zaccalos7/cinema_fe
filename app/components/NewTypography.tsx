8cb8b54bc8a9da1dcbd2601fca308241ce7100cd
Marco Guarino <guarin719@outlook.it>
2026-02-06 11:45:08 +0100

    Added new prop On NewTypography

--- app/components/NewTypography.tsx
+++ app/components/NewTypography.tsx
@@ -1,3 +1,4 @@
+import {Children} from 'react'
 import {cn} from '~/libs/cn'
 
 export interface NewTypographyProps {
@@ -45,16 +46,190 @@ const colorsMapper = {
 const wrapperJsx: {
   [key: string]: React.FC<any>
 } = {
-  h1: ({children, ref, computedAdditionalClassName}) => {
+  h1: ({children, ref, computedAdditionalClassName, onClick, style}) => {
     const mergedClassNames = cn([
       'scroll-m-20 text-4xl font-extrabold tracking-tight',
       computedAdditionalClassName
     ])
 
     return (
-      <h1 ref={ref} className={mergedClassNames}>
+      <h1 ref={ref} className={mergedClassNames} onClick={onClick} style={style}>
         {children}
       </h1>
+    )
+  },
+
+  h2: ({children, ref, computedAdditionalClassName, onClick, style}) => {
+    const mergedClassNames = cn([
+      'scroll-m-20 text-4xl font-extrabold tracking-tight',
+      computedAdditionalClassName
+    ])
+    return (
+      <h2 ref={ref} className={mergedClassNames} onClick={onClick} style={style}>
+        {children}
+      </h2>
+    )
+  },
+
+  h3: ({children, ref, computedAdditionalClassName, onClick, style}) => {
+    const mergedClassNames = cn([
+      'scroll-m-20 text-4xl font-extrabold tracking-tight',
+      computedAdditionalClassName
+    ])
+    return (
+      <h3 className={mergedClassNames} ref={ref} onClick={onClick} style={style}>
+        {children}
+      </h3>
+    )
+  },
+
+  h4: ({children, ref, computedAdditionalClassName, onClick, style}) => {
+    const mergedClassNames = cn([
+      'scroll-m-20 text-4xl font-extrabold tracking-tight',
+      computedAdditionalClassName
+    ])
+    return (
+      <h4 className={mergedClassNames} ref={ref} onClick={onClick} style={style}>
+        {children}
+      </h4>
+    )
+  },
+
+  h5: ({children, ref, computedAdditionalClassName, onClick, style}) => {
+    const mergedClassNames = cn([
+      'scroll-m-20 text-4xl font-extrabold tracking-tight',
+      computedAdditionalClassName
+    ])
+    return (
+      <h5 className={mergedClassNames} ref={ref} onClick={onClick} style={style}>
+        {children}
+      </h5>
+    )
+  },
+  h6: ({children, ref, computedAdditionalClassName, onClick, style}) => {
+    const mergedClassNames = cn([
+      'scroll-m-20 text-4xl font-extrabold tracking-tight',
+      computedAdditionalClassName
+    ])
+    return (
+      <h6 className={mergedClassNames} ref={ref} onClick={onClick} style={style}>
+        {children}
+      </h6>
+    )
+  },
+
+  p: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn([
+      'leading-7 [&:not(:first-child)]:mt-6',
+      computedAdditionalClassNames
+    ])
+
+    return (
+      <p ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </p>
+    )
+  },
+  blockquote: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn(['mt-6 border-l-2 pl-6 italic', computedAdditionalClassNames])
+
+    return (
+      <blockquote ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </blockquote>
+    )
+  },
+  inlineCode: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn([
+      'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
+      computedAdditionalClassNames
+    ])
+
+    return (
+      <code ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </code>
+    )
+  },
+  lead: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn(['text-xl text-muted-foreground', computedAdditionalClassNames])
+
+    return (
+      <p ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </p>
+    )
+  },
+  large: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn(['text-lg font-semibold', computedAdditionalClassNames])
+
+    return (
+      <span ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </span>
+    )
+  },
+  small: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn(['text-sm font-medium leading-none', computedAdditionalClassNames])
+
+    return (
+      <small ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </small>
+    )
+  },
+  muted: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn(['text-sm text-muted-foreground', computedAdditionalClassNames])
+
+    return (
+      <span ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </span>
+    )
+  },
+  body1: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn(['text-base', computedAdditionalClassNames])
+
+    return (
+      <span ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </span>
+    )
+  },
+  body2: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn(['text-sm', computedAdditionalClassNames])
+
+    return (
+      <span ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </span>
+    )
+  },
+  body3: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn(['text-xs', computedAdditionalClassNames])
+
+    return (
+      <span ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </span>
+    )
+  },
+  caption: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn(['leading-5 text-xs text-gray-600', computedAdditionalClassNames])
+
+    return (
+      <p ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </p>
+    )
+  },
+  custom: ({children, computedAdditionalClassNames, style, onClick, ref}) => {
+    const mergedClassNames = cn([computedAdditionalClassNames])
+
+    return (
+      <span ref={ref} onClick={onClick} className={mergedClassNames} style={style}>
+        {children}
+      </span>
     )
   }
 }
@@ -69,7 +244,8 @@ const NewTypography = ({
   color,
   uppercase,
   inLine,
-  asLink
+  asLink,
+  onClick
 }: NewTypographyProps) => {
   const additionalClassNames: String[] = []
 
@@ -100,7 +276,7 @@ const NewTypography = ({
   let computedAdditionalClassName = additionalClassNames.join(' ')
   computedAdditionalClassName += ` ${className}`
 
-  return wrapperJsx[variant]({children, ref, computedAdditionalClassName})
+  return wrapperJsx[variant]({children, ref, computedAdditionalClassName, style, onClick})
 }
 
 export default NewTypography
