3a92e4a4a4b830fe9a04c7647daf9e7dcb316c40
zaccalos7 <guarin719@outlook.it>
2026-02-10 00:07:28 +0100

    added index,ts for centralized import

---
+++ app/components/index.ts
@@ -0,0 +1,10 @@
+// COMPONENT
+export {NewButton} from './NewButton'
+export {NewDiv} from './NewDiv'
+export {NewIcon} from './NewIcon'
+export {NewTooltip} from './NewTooltip'
+export {NewTypography} from './NewTypography'
+
+// PRIMITIVE
+export {Button} from './ui/button'
+export {Tooltip} from './ui/tooltip'
