89516d6f14a3c9feb2728d35a550fd8c2a3651f9
zaccalos7 <guarin719@outlook.it>
2026-02-10 18:45:49 +0100

    Global, index, added NewInput export

--- app/components/index.ts
+++ app/components/index.ts
@@ -4,6 +4,7 @@ export {NewDiv} from './NewDiv'
 export {NewIcon, type IconNames as NewIconNames, type NewIconProps} from './NewIcon'
 export {NewTooltip} from './NewTooltip'
 export {NewTypography} from './NewTypography'
+export {NewInput} from './NewInput'
 
 // PRIMITIVE
 export {Button} from './ui/button'
