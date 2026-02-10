ebf845f10f5d2afb120fa10f6abc7238217b4070
zaccalos7 <guarin719@outlook.it>
2026-02-10 19:59:44 +0100

    Global , label, added new component

--- app/components/ui/label.tsx
+++ app/components/ui/label.tsx
@@ -6,10 +6,7 @@ const labelVariants = cva(
   'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
 )
 
-type LabelProps = {
-  ref?: React.RefObject<HTMLLabelElement>
-  className?: string
-}
+type LabelProps = React.ComponentPropsWithRef<typeof LabelPrimitive.Root>
 
 const Label = ({ref, className, ...props}: LabelProps) => (
   <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
