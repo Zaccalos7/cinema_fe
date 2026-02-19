5deab9d4edcdf1c739d8088118084b7e355dd571
Marco Guarino <guarin719@outlook.it>
2026-02-19 11:44:05 +0100

    Global, login add stuff

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,21 @@
 import {NewDiv, NewInput} from '~/components'
 
 const LoginPage = () => {
-  return <NewDiv className="h-full w-full  gap-4 p-2 bg-orange-400"></NewDiv>
+  return (
+    <NewDiv
+      className="h-full w-full  gap-4 p-2 items-center justify-center bg-orange-400"
+      direction="column"
+    >
+      <NewDiv
+        bordered
+        className="h-1/2 w-1/3 gap-10 p-2 items-center justify-center bg-black"
+        direction="column"
+      >
+        <NewInput />
+        <NewInput />
+      </NewDiv>
+    </NewDiv>
+  )
 }
 
 export default LoginPage
