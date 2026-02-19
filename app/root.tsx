fba9b7af6cc3dab7f728873d1a652332627357f6
Marco Guarino <guarin719@outlook.it>
2026-02-19 09:44:24 +0100

    Global, root.tsx removed h-full on header

--- app/root.tsx
+++ app/root.tsx
@@ -4,7 +4,7 @@ import './mainstyle.css'
 
 export const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
