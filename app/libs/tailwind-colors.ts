d9a21f4e21a9574efb16e8f5cd9d85ffc72a5615
zaccalos7 <guarin719@outlook.it>
2026-02-22 15:37:47 +0100

    global, tailwind-colors

---
+++ app/libs/tailwind-colors.ts
@@ -0,0 +1,29 @@
+import colors from 'tailwindcss/colors'
+
+//documentations https://tailwindcss.com/docs/customizing-colors
+export const amber = colors.amber
+export const black = colors.black
+export const blue = colors.blue
+export const current = colors.current
+export const cyan = colors.cyan
+export const emerald = colors.emerald
+export const fuchsia = colors.fuchsia
+export const gray = colors.gray
+export const green = colors.green
+export const indigo = colors.indigo
+export const lime = colors.lime
+export const neutral = colors.neutral
+export const orange = colors.orange
+export const pink = colors.pink
+export const purple = colors.purple
+export const red = colors.red
+export const rose = colors.rose
+export const sky = colors.sky
+export const slate = colors.slate
+export const stone = colors.stone
+export const teal = colors.teal
+export const transparent = colors.transparent
+export const violet = colors.violet
+export const white = colors.white
+export const yellow = colors.yellow
+export const zinc = colors.zinc
