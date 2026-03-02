34013efea3a82e9a5fa7cee9472554343414a625
marco guarino <m.guarino@mynext.it>
2026-03-02 18:02:06 +0100

    Twitch, handler, created sideBar

--- app/___twitch/handler.tsx
+++ app/___twitch/handler.tsx
@@ -1,33 +1,44 @@
-import {Outlet} from 'react-router'
+import {Outlet, useNavigate} from 'react-router'
 import {NewButton, NewDiv} from '~/components'
 
 const HandleLive = () => {
+  const navigate = useNavigate()
+
   return (
-    <NewDiv className="w-full h-full p-4 gap-4">
+    <NewDiv className="w-full h-full p-4 gap-1" direction="row">
       <NewDiv
-        className="h-full w-1/6 items-center justify-start bg-violet-400 border-2
+        className="h-full w-1/6 items-center justify-start bg-sidebar-foreground bg-indigo-950 border-2
            border-violet-950 p-4 "
         direction="column"
         bordered
       >
-        <NewDiv className="w-full h-full items-center justify-around" direction="column">
+        <NewDiv className="w-full h-full gap-4 p-2" direction="column">
           <NewButton
-            className="bg-pink-500"
+            className="w-full  bg-indigo-950 border-2 border-purple-200"
             type="button"
             label="Gestisci live"
             iconName="list-video"
+            onClick={() => {
+              navigate('./live')
+            }}
           />
           <NewButton
-            className="bg-fuchsia-500"
+            className="w-full bg-indigo-950 border-2 border-purple-200"
             type="button"
             label="Task Manager"
             iconName="monitor-cog"
+            onClick={() => {
+              navigate('./taskManager')
+            }}
           />
           <NewButton
-            className="bg-gray-500"
+            className="w-full bg-indigo-950 border-2 border-purple-200"
             type="button"
             label="Impostazioni"
             iconName="user-cog"
+            onClick={() => {
+              navigate('./setting')
+            }}
           />
         </NewDiv>
       </NewDiv>
