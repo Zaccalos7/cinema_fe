6da09efc01fab16996278f9dc3958c670717cbce
marco guarino <m.guarino@mynext.it>
2026-03-02 18:02:32 +0100

    Twitch, handler.setting, added title and radio

--- app/___twitch/handler.setting.tsx
+++ app/___twitch/handler.setting.tsx
@@ -1,7 +1,21 @@
-import {NewDiv} from '~/components'
+import {NewDiv, NewRadioButtons, NewTypography} from '~/components'
+import { type RadioItem } from '~/components/NewRadioButtons'
 
 const HandlerSetting = () => {
-  return <NewDiv>ciao da settings</NewDiv>
+
+  const options: RadioItem[] = [
+    {label: 'Twitch', value: 'twitch', radioIconProps: {iconName: 'twitch', size: 'medium'}},
+    {label: 'YouTube', value: 'youtube', radioIconProps: {iconName: 'youtube', size: 'medium'}}
+  ]
+
+  return <NewDiv className='w-5/6 h-full'>
+    <NewDiv className='h-1/6 w-full items-center justify-center p-2 gap-4' direction='column'>
+      <NewTypography variant='h2' >{'Impostazioni Diretta'}</NewTypography>
+      <NewRadioButtons options={options} orientation='horizontal' labelClassName='text-xl' defaultValue='twitch'/>
+    </NewDiv>
+
+    
+  </NewDiv>
 }
 
 export default HandlerSetting
