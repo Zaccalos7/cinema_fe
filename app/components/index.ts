8200047efda4cf5e4036e18825b14711ed6c3a31
marco guarino <m.guarino@mynext.it>
2026-03-02 18:00:03 +0100

    Global, index.ts, added new type and components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,10 +1,13 @@
 // COMPONENT
 export {NewButton} from './NewButton'
 export {NewDiv} from './NewDiv'
-export {NewIcon, type IconNames as NewIconNames, type NewIconProps} from './NewIcon'
+export {NewIcon, type IconNames as NewIconNames, type NewIconProps, sizes} from './NewIcon'
 export {NewTooltip} from './NewTooltip'
 export {NewTypography} from './NewTypography'
 export {NewInput} from './NewInput'
+export {NewLabel} from './NewLabel'
+export {NewRadioButtons, type RadioItem} from './NewRadioButtons'
+
 
 // PRIMITIVE
 export {Button} from './ui/button'
