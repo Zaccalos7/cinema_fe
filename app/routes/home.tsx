c86953cce8415c40617bffed66a227a5e0479b7d
zaccalos7 <guarin719@outlook.it>
2026-03-10 15:57:57 +0100

    test for component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,26 @@
 import {NewDiv} from '~/components/NewDiv'
 import type {Route} from './+types/home'
 
+import {useEffect} from 'react'
+
+import {useToast} from '~/components/NewToast'
+
 //on mac
 const Home = () => {
+  const {toast} = useToast()
+
+  useEffect(() => {
+    setTimeout(() => {
+      toast({
+        title: 'ciao',
+        variant: 'success'
+      })
+    })
+  }, [])
+
   return (
     <>
-      <NewDiv className="bg-red-500">ciao</NewDiv>
+      <NewDiv className="w-full h-full" direction="column"></NewDiv>
     </>
   )
 }
