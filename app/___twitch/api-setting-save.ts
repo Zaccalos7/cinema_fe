622deb5513061714a420a129e5f7ed3a5181c033
zaccalos7 <guarin719@outlook.it>
2026-03-11 21:56:40 +0100

    twitch added api for save configurations

--- app/___twitch/api-setting-save.ts
+++ app/___twitch/api-setting-save.ts
@@ -3,16 +3,15 @@ import {type Route} from './+types/api-setting-save'
 const BASE_URL = 'http://localhost:1200/settings/save'
 
 export const action = async ({request, context}: Route.ActionArgs) => {
-
   const formData = await request.formData()
-  
-  const data =  formData.get('data') as string
+
+  const data = formData.get('data') as string
 
   const parsedData = JSON.parse(data)
 
   console.log(parsedData)
 
-    const response = await fetch(BASE_URL, {
+  const response = await fetch(BASE_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -20,7 +19,11 @@ export const action = async ({request, context}: Route.ActionArgs) => {
     body: JSON.stringify(parsedData)
   })
 
-  console.log({response})
+  const messageResponse: {message: string; response: string} = await response.json()
+  console.log({messageResponse})
 
-  return {}
+  return {
+    message: messageResponse.message,
+    response: messageResponse.response
+  }
 }
