87117a5a8954ece916185455d908ecf154ba115f
zaccalos7 <guarin719@outlook.it>
2026-03-11 01:10:45 +0100

    preparing call to backend

---
+++ app/___twitch/api-setting-save.ts
@@ -0,0 +1,5 @@
+import {type Route} from './+types/api-setting-save'
+
+export const action = async ({request, context}: Route.ActionArgs) => {
+  return {}
+}
---
+++ app/___twitch/types/settingTypes.ts
@@ -0,0 +1,5 @@
+export interface SettingRecord {
+  streamUrl: string
+  streamKey: string
+  platformStreamName: string
+}
