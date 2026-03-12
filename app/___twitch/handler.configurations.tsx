3c5b40ca90805e22b4eddd4a1564ec9b7eed8c16
marco guarino <m.guarino@mynext.it>
2026-03-12 18:09:27 +0100

    Twitch, handler.configurations, added list of configurations

--- app/___twitch/handler.configurations.tsx
+++ app/___twitch/handler.configurations.tsx
@@ -1,24 +1,57 @@
 import {useState} from 'react'
 import {useFetcher} from 'react-router'
-import {NewDiv, NewSelect} from '~/components'
+import {NewCard, NewDiv, NewSelect} from '~/components'
+import './styles/backGround.css'
+import { Route } from './+types/handler.configurations'
 
-const UserConfigurations = () => {
+const BASE_URL = 'http://localhost:1200/settings/retrive'
+
+export const loader = async ({request}: Route.LoaderArgs)=> {
+  // const url = new URL(request.url)
+  // const searchParams = url.searchParams
+
+  // const streamUrl = searchParams.get('streamUrl') ?? ''
+  // const platformStreamName = searchParams.get('platformStreamName') ?? ''
+  // const URL_API = `${BASE_URL}?streamUrl=${streamUrl}&platformStreamName=${platformStreamName}`
+
+  console.log('Loader called with URL:', BASE_URL)
+
+  const response = await fetch(BASE_URL)
+
+  console.log('Response from server:', response)
+   
+  if(!response?.ok) {
+    console.error('Error response from server:')
+    return
+  } 
+
+
+  const configurationsList = await response.json() ?? []
+
+  console.log('Configurations List:', configurationsList)
+
+  return {
+    configurationsList
+  }
+
+}
+
+const UserConfigurations = ({loaderData}: Route.ComponentProps) => {
   const retriveConfigurationsFetcher = useFetcher()
 
-  const options = [
-    {
-      label: 'pippo',
-      value: '2'
-    },
-    {
-      label: 'gino',
-      value: '2'
-    }
-  ]
 
   return (
-    <NewDiv className="h-full w-full">
-      <NewSelect options={options} />
+    <NewDiv className="h-full w-full gap-2 handlerSettingsStyle" direction="column">
+      {JSON.stringify(loaderData)}
+      <NewDiv className="h-5/12 w-1/3 items-center justify-center">
+        <NewCard
+          title="Twitch Configurations"
+          description="Configure your Twitch account and settings."
+          width={200}
+        >
+          ciao
+        </NewCard>
+      </NewDiv>
     </NewDiv>
   )
 }
