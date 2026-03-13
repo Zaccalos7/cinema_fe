b376e5e51176e6caf4f0ade4397e994f7bc32518
marco guarino <m.guarino@mynext.it>
2026-03-13 22:20:15 +0100

    NewCard, addedtitlesizemap

--- app/components/NewCard.tsx
+++ app/components/NewCard.tsx
@@ -8,33 +8,48 @@ interface NewCardProps {
   description?: React.ReactNode
   children?: React.ReactNode
   footer?: React.ReactNode
-  className?: string
+  className?: string,
+  titleSize?: keyof typeof TitleSizeConst
 }
 
-const NewCard = ({width, title, description, children, footer, className}: NewCardProps) => {
+const TitleSizeConst = {
+  small: 'text-sm',
+  medium: 'text-base',
+  large: 'text-lg',
+  extraLarge: 'text-xl'
+}
+
+const NewCard = ({width, title, description, children, footer, className, titleSize}: NewCardProps ) => {
   return (
-    <Card className={cn('w-full overflow-hidden gap-2', className)} style={{...(width && {width})}}>
-      <NewDiv className={cn('w-full flex flex-col', className)} align="start" justify="start">
+    <Card
+      className={cn('w-full h-full overflow-hidden flex flex-col gap-2 ', className)}
+      style={{...(width && {width})}}
+    >
+      <NewDiv className="w-full h-full flex flex-col flex-1 " align="start" justify="start">
         {(title || description) && (
-          <CardHeader className="p-2 flex flex-col w-full">
+          <CardHeader className="h-[30%] p-2 flex flex-col w-full bg-yellow-400">
             {title && (
-              <CardTitle className="w-full break-words text-base font-semibold text-center">
+              <CardTitle className={`h-full w-full break-words text-base font-semibold text-center ${titleSize ? TitleSizeConst[titleSize] : TitleSizeConst.medium} bg-green-500 `}>
                 {title}
               </CardTitle>
             )}
 
             {description && (
-              <CardDescription className="w-full text-sm text-muted-foreground break-words">
+              <CardDescription className="w-full text-sm text-muted-foreground break-words bg-blue-400">
                 {description}
               </CardDescription>
             )}
           </CardHeader>
         )}
 
-        {children && <CardContent className="p-2 w-full">{children}</CardContent>}
-
-        {footer && <CardFooter className="p-2 w-full">{footer}</CardFooter>}
+        {children && (
+          <CardContent className="p-2 w-full flex-1">
+            {children}
+          </CardContent>
+        )}
       </NewDiv>
+
+      {footer && <CardFooter className="p-2 w-full">{footer}</CardFooter>}
     </Card>
   )
 }
