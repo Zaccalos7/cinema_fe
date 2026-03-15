35b9647e79a7dff88260a338bf57de0be07bef71
zaccalos7 <guarin719@outlook.it>
2026-03-16 00:48:46 +0100

    added page for handled system info

--- app/___twitch/handler.taskManager.tsx
+++ app/___twitch/handler.taskManager.tsx
@@ -1,12 +1,28 @@
 import {NewDiv, NewIcon, NewTypography} from '~/components'
-import {green, pink} from '~/libs/tailwind-colors'
+import {green, orange, pink, violet} from '~/libs/tailwind-colors'
 import {Route} from './+types/handler.taskManager'
+import {IconName} from 'types/icons'
+import {SystemInfoDto} from './types/SystemInfoTypes'
+import {useRevalidator} from 'react-router'
+import {useEffect} from 'react'
+
+interface InfoCellBlockType {
+  iconName: IconName
+  cellName: string
+  cellValue: number
+  iconColor: string
+}
 
 export const loader = async ({request}: Route.LoaderArgs) => {
-  const response = await fetch('http://localhost:1200/taskManager/statistics/cpu')
-  const value = await response.json()
-  console.log(value)
-  return {cpu: value}
+  const responseAll = await fetch('http://localhost:1200/taskManager/statistics/allInfo')
+  const systemInfoList: SystemInfoDto[] = await responseAll.json()
+
+  const cpuInfo = systemInfoList?.[0]
+  const ramInfo = systemInfoList?.[1]
+  const swapInfo = systemInfoList?.[2]
+  const cpuTemperatureInfo = systemInfoList?.[3]
+
+  return {cpuInfo, ramInfo, swapInfo, cpuTemperatureInfo}
 }
 
 const colorLevel = (value: number) => {
@@ -27,110 +43,83 @@ const colorLevel = (value: number) => {
   }
 }
 
+const InfoCellBlock = ({iconName, cellName, cellValue, iconColor}: InfoCellBlockType) => {
+  const toTransitionsColor = colorLevel(cellValue)
+
+  return (
+    <NewDiv
+      className="h-1/3 w-1/2 bg-indigo-950 border-2 border-violet-950 px-4 py-2 gap-4 flex justify-center items-center"
+      bordered
+    >
+      <NewDiv className="h-2/5 w-1/5 rounded-full flex items-center justify-center bg-indigo-900 ">
+        <NewIcon name={iconName} size={100} className="animate-pulse" color={iconColor} />
+      </NewDiv>
+
+      <NewDiv className="h-3/5 w-full flex flex-col justify-center gap-3">
+        <NewTypography className="text-white text-5xl animate-gradient">{cellName}</NewTypography>
+
+        <NewTypography className="text-right font-extrabold text-2xl text-zinc-200">
+          {cellValue}%
+        </NewTypography>
+
+        <NewDiv className="w-full h-[14px] bg-slate-300 rounded-full overflow-hidden shadow-inner">
+          <NewDiv
+            className={`h-full rounded-full transition-all duration-700 ease-out bg-gradient-to-r from-blue-400 ${toTransitionsColor}`}
+            style={{width: `${cellValue}%`}}
+          />
+        </NewDiv>
+      </NewDiv>
+    </NewDiv>
+  )
+}
+
 const HandlerTaskManager = ({loaderData}: Route.ComponentProps) => {
-  const toTransitionsColor = colorLevel(loaderData.cpu)
+  const {cpuInfo, ramInfo, swapInfo, cpuTemperatureInfo} = loaderData
+
+  const revalidator = useRevalidator()
+
+  useEffect(() => {
+    if (!revalidator) {
+      return
+    }
+
+    const interval = setInterval(() => {
+      revalidator.revalidate()
+    }, 5_000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <NewDiv className="h-full w-5/6 flex-wrap items-start justify-start">
       <NewDiv className="h-full w-full flex flex-wrap items-start p-2 gap-4 " direction="column">
-        {/* CPU */}
-        <NewDiv
-          className="h-1/3 w-1/2 bg-indigo-950 border-2 border-violet-950 px-4 py-2 gap-4 flex justify-center items-center"
-          bordered
-        >
-          <NewDiv className="h-2/5 w-1/5 rounded-full flex items-center justify-center bg-indigo-900 ">
-            <NewIcon name="brain" size={100} className="animate-pulse" color={pink[300]} />
-          </NewDiv>
+        <InfoCellBlock
+          cellName={cpuInfo.field}
+          cellValue={cpuInfo.value}
+          iconName="brain"
+          iconColor={pink[300]}
+        />
 
-          <NewDiv className="h-3/5 w-full flex flex-col justify-center gap-3">
-            <NewTypography className="text-white text-5xl animate-gradient">CPU</NewTypography>
+        <InfoCellBlock
+          cellName={ramInfo.field}
+          cellValue={ramInfo.value}
+          iconName="memory-stick"
+          iconColor={green[300]}
+        />
 
-            <NewTypography className="text-right font-extrabold text-2xl text-zinc-200">
-              {loaderData.cpu}%
-            </NewTypography>
+        <InfoCellBlock
+          cellName={swapInfo.field}
+          cellValue={swapInfo.value}
+          iconName="database"
+          iconColor={orange[300]}
+        />
 
-            <NewDiv className="w-full h-[14px] bg-slate-300 rounded-full overflow-hidden shadow-inner">
-              <NewDiv
-                className={`h-full rounded-full transition-all duration-700 ease-out bg-gradient-to-r from-blue-400 ${toTransitionsColor}`}
-                style={{width: `${loaderData.cpu}%`}}
-              />
-            </NewDiv>
-          </NewDiv>
-        </NewDiv>
-
-        {/* RAM */}
-        <NewDiv
-          className="h-1/3 w-1/2 bg-indigo-950 border-2 border-violet-950 px-4 py-2 gap-4 flex"
-          bordered
-        >
-          <NewDiv className="h-full w-1/5 flex items-center justify-center">
-            <NewIcon name="memory-stick" size={120} color={green[300]} className="animate-pulse" />
-          </NewDiv>
-
-          <NewDiv className="h-full w-4/5 flex flex-col justify-center gap-3">
-            <NewTypography className="text-white text-5xl">RAM</NewTypography>
-
-            <NewTypography className="text-right font-extrabold text-2xl text-zinc-200">
-              0%
-            </NewTypography>
-
-            <NewDiv className="w-full h-[14px] bg-slate-300 rounded-full overflow-hidden shadow-inner">
-              <NewDiv
-                className="h-full rounded-full transition-all duration-700 ease-out bg-gradient-to-r from-emerald-400 to-cyan-300"
-                style={{width: '0%'}}
-              />
-            </NewDiv>
-          </NewDiv>
-        </NewDiv>
-
-        {/* SWAP */}
-        <NewDiv
-          className="h-1/3 w-1/2 bg-indigo-950 border-2 border-violet-950 px-4 py-2 gap-4 flex"
-          bordered
-        >
-          <NewDiv className="h-full w-1/5 flex items-center justify-center">
-            <NewIcon name="database" size={120} className="animate-pulse" />
-          </NewDiv>
-
-          <NewDiv className="h-full w-4/5 flex flex-col justify-center gap-3">
-            <NewTypography className="text-white text-5xl">SWAP</NewTypography>
-
-            <NewTypography className="text-right font-extrabold text-2xl text-zinc-200">
-              0%
-            </NewTypography>
-
-            <NewDiv className="w-full h-[14px] bg-slate-300 rounded-full overflow-hidden shadow-inner">
-              <NewDiv
-                className="h-full rounded-full transition-all duration-700 ease-out bg-gradient-to-r from-pink-400 to-yellow-300"
-                style={{width: '0%'}}
-              />
-            </NewDiv>
-          </NewDiv>
-        </NewDiv>
-
-        {/* TEMP */}
-        <NewDiv
-          className="h-1/3 w-1/2 bg-indigo-950 border-2 border-violet-950 px-4 py-2 gap-4 flex"
-          bordered
-        >
-          <NewDiv className="h-full w-1/5 flex items-center justify-center">
-            <NewIcon name="thermometer" size={120} className="animate-pulse" />
-          </NewDiv>
-
-          <NewDiv className="h-full w-4/5 flex flex-col justify-center gap-3">
-            <NewTypography className="text-white text-5xl">TEMP</NewTypography>
-
-            <NewTypography className="text-right font-extrabold text-2xl text-zinc-200">
-              0°C
-            </NewTypography>
-
-            <NewDiv className="w-full h-[14px] bg-slate-300 rounded-full overflow-hidden shadow-inner">
-              <NewDiv
-                className="h-full rounded-full transition-all duration-700 ease-out bg-gradient-to-r from-rose-400 to-pink-200"
-                style={{width: '0%'}}
-              />
-            </NewDiv>
-          </NewDiv>
-        </NewDiv>
+        <InfoCellBlock
+          cellName={cpuTemperatureInfo.field}
+          cellValue={cpuTemperatureInfo.value}
+          iconName="thermometer"
+          iconColor={violet[600]}
+        />
       </NewDiv>
     </NewDiv>
   )
